936bd02db9695dcc471fb9951cd9749fda7aa49c
chappa <pallavichappa04@gmail.com>
2025-05-20 20:13:38 +0530

    MERN

--- React/bookCrud/routes/bookRoutes.js
+++ React/bookCrud/routes/bookRoutes.js
@@ -1,31 +1,17 @@
-const express=require("express")
-const router=express.Router();
-const Book=require("../models/bookSchema")
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/bookSchema');
 
-
-//get all books
-
-router.get("/",async(req,res)=>{
-    try{
-        const books=await Book.find();
-        if(!books){
-            return res.status(404).json({message:"No books found"});
+// GET all books
+router.get('/', async (req, res) => {
+    try {
+        const books = await Book.find();
+        if (books.length === 0) {
+            return res.status(404).json({ message: "No books found" });
         }
-        res.status(200).json({message:"Books fetchd successfully",books});
-    
-    }catch(error){
-        res.status(500).json({message:error.message});
+        res.status(200).json({ message: "Books fetched successfully", books });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
-
-module.exports=router;
-
-//post
-
-
-//put
-
-
-//delete
-
-//get
+module.exports = router;
