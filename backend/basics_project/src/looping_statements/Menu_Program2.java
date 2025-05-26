48630344c5e1e7b7ff0dbab5e4a534af55973d73
chappa <pallavichappa04@gmail.com>
2025-05-26 19:39:11 +0530

    Java

--- backend/basics_project/src/looping_statements/Menu_Program2.java
+++ backend/basics_project/src/looping_statements/Menu_Program2.java
@@ -11,9 +11,20 @@ public class Menu_Program2 {
 			int choice=sc.nextInt();
 			switch (choice) {
 			case 1:{
-				System.out.println("Mens Wear");
-			}	
-				break;
+				boolean mensCheck=true;
+				do {
+					System.out.println("Choose mnes wear 1.shirt 2.pant 3.shoes 4 exit");
+					int menschoice=sc.nextInt();
+					switch(menschoice) {
+					case 1:System.out.println("shirt");break;
+					case 2:System.out.println("pant");break;
+					case 3:System.out.println("shoes");break;
+					case 4:mensCheck=false;break;
+
+					}
+				}while(mensCheck);
+							
+			}break;
 
 			case 2:{
 				System.out.println("Womens Wear");
