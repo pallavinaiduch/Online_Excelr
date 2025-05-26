c741c5cb22cf8a2438a4ab125fc6cb75bedfd687
chappa <pallavichappa04@gmail.com>
2025-05-26 20:00:53 +0530

    Java

---
+++ backend/basics_project/src/looping_statements/WhileProgram2.java
@@ -0,0 +1,15 @@
+package looping_statements;
+
+public class WhileProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char value=65;
+		while(value<=90) {
+		System.out.println(value);
+		value++;
+		}
+
+	}
+
+}
