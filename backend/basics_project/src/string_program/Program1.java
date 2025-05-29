800b1f2ed1a1a9c4d9e087e7c0309c68ff690c37
chappa <pallavichappa04@gmail.com>
2025-05-29 18:51:36 +0530

    strings

---
+++ backend/basics_project/src/string_program/Program1.java
@@ -0,0 +1,47 @@
+package string_program;
+
+
+import java.lang.String;
+import java.util.Arrays;
+
+public class Program1 {
+     public static void main(String[] args) {
+		String a="raj kumar";
+		String b=new String ("raj kumar");
+		
+		System.out.println(b);//raj kumar
+		System.out.println(a);//raj kumar
+		
+		String s1="raj";
+		String s2="raj";
+		System.out.println(s1==s2); //true
+		
+		
+		
+		String s3=new String ("raj");
+		String s4=new String("raj");
+		System.out.println(s3==s4);//false
+		
+		
+		
+		String s5="this is a string program";
+		char []arr=s5.toCharArray();
+		System.out.println(Arrays.toString(arr));
+		System.out.println(arr.length);
+		
+		
+		String s6="kumar";
+		System.out.println("size of "+s6+" is " +s6.length());
+		
+		
+		String s7="this is a string program";
+		System.out.println(s7.substring(10, 16));
+		System.out.println(s7.charAt(0));//t
+	  //System.out.println(s7.charAt(89));//SIOBE(String index out of bounds Exception)
+		                                  //RTE(run time exception)
+		System.out.println(s7.indexOf('z'));
+
+
+		
+	}
+}
---
+++ backend/basics_project/src/string_program/Program2.java
@@ -0,0 +1,49 @@
+package string_program;
+
+import java.util.Arrays;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		  String s="hello java";
+		  System.out.println(s.substring(6));//java
+		  System.out.println(s.substring(6,10));//java
+//		  to check value is present or not 
+		  System.out.println(s.indexOf('a'));//7
+		  System.out.println(s.lastIndexOf('a'));//9
+		  System.out.println(s.contains("java"));//true
+		  System.out.println(s.startsWith("hi"));//false
+		  System.out.println(s.endsWith("va"));//true
+		  
+		  
+		  System.out.println(s.toUpperCase()); //HELLO JAVA
+		  String s2=" HELLO JaVa ";
+		  System.out.println(s2.toLowerCase());//hello java
+		  
+//		  removes whites paces 
+		  System.out.println(s2.length());//12
+		  String res = s2.trim();
+		  System.out.println(res.length());//10
+		  System.out.println(res);
+		  
+		  
+		  String s3="this is java program";
+		  String[] arr = s3.split(" ");
+		  System.out.println(Arrays.toString(arr));//[this, is, java, program]
+
+		  
+		  String s4="this is Java program";
+		  String s5="this is java program";
+		  System.out.println(s4.equals(s5));//false
+		  System.out.println(s4.equalsIgnoreCase(s5));//true
+		  
+		  System.out.println(s4.matches(".*Java.*"));
+		  
+//		  based on unicode/ASCII values
+		  System.out.println("Dog".compareTo("Dog")); //0 both are same
+		  System.out.println("Cat".compareTo("Dog")); //-1
+		  System.out.println("Dog".compareTo("Cat"));  //1
+		  
+
+	}
+}
