aaf3d19918c3dbdaa35b7ad445a7ddf211fb75d5
chappa <pallavichappa04@gmail.com>
2025-05-29 13:45:48 +0530

    Arrays

--- backend/basics_project/src/array_programs/Array_Program5.java
+++ backend/basics_project/src/array_programs/Array_Program5.java
@@ -5,21 +5,27 @@ import java.util.Arrays;
 public class Array_Program5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[]arr= {1,2,3};
+//		 represents in the form of rows, col in {}
+		int [] arr3= {1,2};
 		int [] [] arr2= {
 				{1,2,3,4},
 				{5,6,7,8}
-		};
-		
-		
-		System.out.println(Arrays.toString(arr2));  //[address,address]
+				};
+	
+		System.out.println(Arrays.toString(arr2));//[address, address]
 		System.out.println(Arrays.deepToString(arr2));
-		for (int i=0;i<arr2.length;i++) {
-			for(int j=0;j<arr2[i].length;j++) {
-				System.out.print(arr2[i][j]+" ");
+		
+		System.out.println(arr2.length);
+		System.out.println(arr2[0].length);
+		System.out.println(arr2[1].length);
+			
+		for(int i=0;i<arr2.length;i++) {
+			 for (int j = 0; j < arr2[i].length; j++) {
+				 System.out.print(arr2[i][j]+" ");
+				
 			}
-			System.out.println();
+			 System.out.println();
 		}
 	}
-
 }
--- backend/basics_project/src/array_programs/Array_Program6.java
+++ backend/basics_project/src/array_programs/Array_Program6.java
@@ -1,10 +1,19 @@
 package array_programs;
 
+import java.util.Iterator;
+
 public class Array_Program6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ public static void main(String[] args) {
+		int [][]arr= {{10,20,30},{40,50,60},{70,80,80}};
+		int [][]arr1= {{100,200,300},{400,500,600},{700,800,800}};
 
-	}
-
+		for(int i=0;i<arr.length;i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.println(arr[i][j]+" + "+arr1[i][i]+" = "+(arr[i][j]+arr1[i][j]));
+			}
+			System.out.println();
+		}
+}
+	
 }
